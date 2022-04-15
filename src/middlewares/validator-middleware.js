3e2153ecf2c4ed76acdd111b5094d7621f37cafc
Luis Carrillo <luis.carrillo@itjuana.com>
2022-04-15 10:52:17 -0500

    Missed conditional chaining

--- src/middlewares/validator-middleware.js
+++ src/middlewares/validator-middleware.js
@@ -3,7 +3,7 @@ const validateRequiredProps = (object, requiredProps) => {
   const obecjtCopy = { ...object };
 
   requiredProps.forEach((propName) => {
-    const prop = obecjtCopy[propName].trim?.() ?? "";
+    const prop = obecjtCopy[propName]?.trim?.() ?? "";
     if (prop === "") {
       invalidProp = propName;
       return;
